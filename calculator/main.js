3f0e3a7721524cbc3bfc36a93e86aad8f21e5017
t19106 <g037sk@gmail.com>
2023-01-13 19:16:44 +0900

    電卓ロジック修正

--- calculator/main.js
+++ calculator/main.js
@@ -3,14 +3,27 @@
 const DISPLAY = document.calculator.display;
 const OPERATOR = ["+", "-", "*", "/"];
 const PATTERN_OP_AND_ZERO       = /[\+\-\*\/]0$/;
-const PATTERN_OP_AND_NUM        = /[\+\-\*\/][0-9]$/;
 const PATTERN_NUM_DOT_NUM       = /^[0-9]+\.[0-9]+/
 const PATTERN_OP_NUM_DOT_NUM_BW = /[\+\-\*\/][0-9]+\.[0-9]+$/;
 const PATTERN_OP_NUM_BW         = /[\+\-\*\/][0-9]+$/;
 
+const ERROR_VALUE = "エラー";
+const INITIAL_VALUE = "0";
+
+// 各機能の仕様については、コード中のコメントをご覧ください。
+// 下記、本電卓に実装されていない仕様を説明します。
+// -------------------------------------------------------- //
+// 1. 通常の電卓では、演算子の後に「=」を入力すると、演算子の前に入力された数字を演算子の後に入力されていることを前提に計算されるが、この機能は持ち合わせていない
+//    例：本電卓では「3+」の後に「=」を入力しても、入力「=」は無効になる（通常の電卓では「6」が表示される）
+// 2. 通常の電卓では、元々の式の結果を求めた後、新たに演算子を入力するまでに「=」を連続して入力すると、結果に対して、式の演算子と、演算子の後に入力された数字を元に新たに計算を行うが、
+//    この機能は持ち合わせていない
+//    例：本電卓では「3+3」の後に「=」を入力して「9」を求めた場合、その後に連続して「=」を入力しても文字列「9」を評価し続ける（通常の電卓では「12」が表示される）
+// -------------------------------------------------------- //
+// 補足：本電卓は、Javascriptの仕様により発生する丸め誤差には対応していない
+
 function inputNaturalNumber (btn) {
     //表示の値が「0」あるいは「エラー」（初期状態）の場合、値を入力値に置き換える
-    if (DISPLAY.value === "0" || DISPLAY.value === "エラー") {
+    if (DISPLAY.value === INITIAL_VALUE || DISPLAY.value === ERROR_VALUE) {
         DISPLAY.value = btn.value;
     // 表示の値が「任意の演算子」と「0」の組み合わせによる後方一致の場合、値の末尾「0」を入力値に置き換える
     // 例：「1+0」の時に「1」を入力した場合、「1+1」となる
@@ -24,7 +37,7 @@ function inputNaturalNumber (btn) {
 }
 function inputZero (btn) {
     // 表示の値が「0」あるいは「エラー」（初期状態）の場合、入力「0」を無効にする
-    if (DISPLAY.value === "0" || DISPLAY.value === "エラー") {
+    if (DISPLAY.value === INITIAL_VALUE || DISPLAY.value === ERROR_VALUE) {
         return 1;
     // 表示の値が「任意の演算子」と「0」の組み合わせによる後方一致の場合、入力「0」を無効にする
     // 例：「1+0」の場合、入力「0」を無効する
@@ -35,11 +48,11 @@ function inputZero (btn) {
     DISPLAY.value += btn.value;
 }
 function clearCalculator () {
-    DISPLAY.value = "0";
+    initialize();
 }
 function getAnswer (btn) {
     // 表示の値が「エラー」の場合、入力「=」を無効にする
-    if (DISPLAY.value === "エラー") {
+    if (DISPLAY.value === ERROR_VALUE) {
         return 1;
     // 表示の値が「任意の演算子」による後方一致の場合、入力「=」を無効にする
     } else if (OPERATOR.includes(DISPLAY.value.slice(-1))) {
@@ -52,7 +65,7 @@ function getAnswer (btn) {
     DISPLAY.value = eval(DISPLAY.value);
     // 答えがNaN、Infinity等に一致する場合、値を「エラー」に置き換える
     if (DISPLAY.value === "NaN" || DISPLAY.value === "Infinity") {
-        DISPLAY.value = "エラー";    
+        DISPLAY.value = ERROR_VALUE;    
     } 
 }
 function inputDot (btn) {
@@ -60,7 +73,7 @@ function inputDot (btn) {
     if (DISPLAY.value.endsWith(btn.value)) {
         return 1;
     // 表示が「0」あるいは「エラー」（初期状態）の場合、値を「0.」に置き換える
-    } else if (DISPLAY.value === "0" || DISPLAY.value === "エラー") {
+    } else if (DISPLAY.value === INITIAL_VALUE || DISPLAY.value === ERROR_VALUE) {
         DISPLAY.value = "0.";
     // 表示が「任意の演算子」による後方一致の場合、値の末尾に「.0」を入力する
     // 例：「1+」の時に「.」を入力した場合、「1+0.」となる
@@ -116,3 +129,9 @@ function inputOperator (btn) {
         DISPLAY.value += btn.value;
     }
 }
+
+// 初回時の電卓初期化
+function initialize () {
+    DISPLAY.value = INITIAL_VALUE;
+}
+initialize();
